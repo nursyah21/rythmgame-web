c6692ecf941ee3ec582477996b348f9309e8e0ba
nursyah21 <nursyahjaya@gmail.com>
2024-02-22 15:26:41 +0700

    update v0.1

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,50 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+
+  // change background
+  let body = document.body;
+  function updateImageUrl(newUrl) {
+    body.style.backgroundImage = `url(${newUrl})`;
+    body.style.backgroundSize = "cover";
+    body.style.backgroundPosition = "center";
+    body.style.backgroundRepeat = "no-repeat";
+  }
+  updateImageUrl("/images/kokoronashi.png");
+
+  // play song
+  let isPlaying = false;
+  let audioSource = "/music/kokoronashi.mp3";
+  let audio;
+  let pause
+  onMount(() => {
+    window.addEventListener("keydown", (event) => {
+      if (event.code === "Space") {
+        isPlaying = !isPlaying;
+      }
+      if (isPlaying) {
+        pause.style.display = 'flex'
+        audio.play();
+      }
+      else {
+        pause.style.display = 'none'
+        audio.pause();
+      }
+    });
+  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <audio src={audioSource} bind:this={audio}></audio>
+  <div bind:this={pause} class="center">
+    <h1>pause</h1>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .center{
+    display: none;
+    justify-content: center;
+    height: 100vh;
+    align-items: center;
   }
 </style>
